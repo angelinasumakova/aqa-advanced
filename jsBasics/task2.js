0832bc42e5807fc6e4e27fff12dc0cba47b8bb93
Anhelina Sumakova <angelina.sumakova@gmail.com>
2025-06-08 22:41:08 +0300

    HomeWork 4.1. JS primitive data types

---
+++ jsBasics/task2.js
@@ -0,0 +1,8 @@
+var name1 = 'John';
+var name2 = 'Morgan';
+var greeting1 = 'Hello Mr. ' + name1 + ' and Mr. ' + name2 + '!';
+var greeting2 = `Hello Mr. ${name1} and Mr. ${name2}!`;
+
+console.log(greeting1);
+console.log(greeting2);
+
